784e879dda266fbca6b8264a0a2bbc1c535520df
Nilesh010101 <nilesh.chauhan4@gmail.com>
2024-06-07 08:19:04 +0530

    learning

---
+++ Exercise07062024/lab0002.py
@@ -0,0 +1,7 @@
+pi = 3.14
+print(pi)
+print(type(pi))
+
+name = "this is big line"
+print(len(name))
+print(name[13])
---
+++ Exercise07062024/lab001.py
@@ -0,0 +1,7 @@
+numb1 = input("Enter the first number")
+numb2 = input("Enter the second number")
+print(numb1 + numb2)
+
+result= int(numb1) + int(numb2)
+print(result)
+
